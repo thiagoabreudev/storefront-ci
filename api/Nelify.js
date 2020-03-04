50ba8f78e1c066ca8502b95c78c561f1be3f8eb9
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2020-03-04 00:14:09 -0300

    feat(netlify): created setp to create netlify site

---
+++ api/Nelify.js
@@ -0,0 +1,52 @@
+const axios = require('axios').create({
+  baseURL: process.env.STOREFRONT_CI_NETLIFY_URL,
+  headers: {
+    'Authorization': `Bearer ${process.env.STOREFRONT_CI_NETLIFY_TOKEN}`
+  }
+});
+
+class Netlify {
+  deploy(payload) {
+    return new Promise((resolve, reject) => {
+      this.createSite(payload)
+      .then(({ data }) => this.updateSiteWithRepo(payload, data))
+      .then(({data}) => resolve(data))
+      .catch(({response}) => reject({
+        setep: 'netlify',
+        status: response.status,
+        error: response.statusText,
+        details: response.data.errors
+      }))
+    })
+  }
+
+  createSite(payload) {
+    return new Promise((resolve, reject) => {
+      const data = { "name": payload.name }
+      axios.post('/sites', data)
+        .then(result => resolve(result))
+        .catch(error => reject(error))
+    })
+  }
+
+  updateSiteWithRepo(payload, siteData) {
+    return new Promise((resolve, reject) => {
+      const owner = payload.owner ? payload.owner : process.env.STOREFRONT_CI_GITHUB_DEFAULT_OWNER;
+      const data = {
+        repo: {
+          provider: process.env.STOREFRONT_CI_NETLIFY_PROVIDER || 'github',
+          installation_id: process.env.STOREFRONT_CI_NETLIFY_GITHUB_INSTALATION_ID,
+          repo: `${owner}/${payload.name}`,
+          private: false,
+          env: { },
+          branch: process.env.STOREFRONT_CI_NETLIFY_BRANCH || 'dist'
+        }
+      }
+      axios.put(`/sites/${siteData.id}`, data)
+        .then(result => resolve(result))
+        .catch(error => reject(error))
+    })
+  }
+}
+
+module.exports = new Netlify();
--- api/deploy.js
+++ api/deploy.js
@@ -1,18 +1,21 @@
 const github = require('./Github');
 const gotrue = require('./Gotrue');
 const gitgateway = require('./GitGateway');
+const netlify = require('./Nelify')
 
 const deploy = (req, res) => {
   Promise.all([
     github.deploy(req.body),
     gotrue.deploy(req.body),
-    gitgateway.deploy(req.body)
+    gitgateway.deploy(req.body),
+    netlify.deploy(req.body)
   ])
     .then(responses => {
       res.json({
         github: responses[0],
         gotrue: responses[1],
-        gitgateway: responses[2]
+        gitgateway: responses[2],
+        netlify: responses[3]
       })
     })
     .catch(error => {
